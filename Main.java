ec6492742c3b2704cbe59e73955cf02edf9efec2
aslibeyhan <77741685+aslibeyhan@users.noreply.github.com>
2023-10-25 01:24:02 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,55 @@
+package org.example;
+
+public class Main {
+   // Java BootCamp 1.Ders
+
+    public static void main(String[]args){
+        // System.out.println("Hello world !");
+        // String metin = "İlginizi çekebilir ";
+        // Javada değişken isimlendirmeleri camelCase yazılır.
+
+        //************ Değişkenler****************************
+
+        String ortaMetin="İlginizi çekebilir" ;
+        String altMetin="Vade süresi";
+        System.out.println(ortaMetin);
+
+        int vade = 12;
+        double dolarDun = 18.10;
+        double dolarBugun= 18.10;
+
+        boolean dolarDustuMu = true; // or false
+
+       //***********Şart Blokları*****************
+
+        String okYonu = "";   //empty
+
+        if (dolarBugun<dolarDun){
+            okYonu="down.svg";
+            System.out.println(okYonu);
+        }else if (dolarBugun>dolarDun){
+             okYonu="up.svg";
+             System.out.println(okYonu);
+        }
+        else {
+            okYonu="equal.svg";
+            System.out.println(okYonu);
+        }
+
+        //*********** Arrray-Diziler **************
+
+        String[] krediler ={"Emekli Kredisi","İşçi Kredisi","Öğrenci Kredisi"};
+
+        //******* Loop - Döngüler ****************
+        for (int i=0;i<krediler.length;i++){
+            System.out.println(krediler[i]);
+        }
+
+
+
+
+
+    }
+
+
+}
